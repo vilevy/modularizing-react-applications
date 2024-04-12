a1f87840a7a2b2bc29005b8459f3c10f1ba009d5
vilevy <victorlevygp@gmail.com>
2024-04-12 07:35:39 -0300

    refactor: split logic to custom hook

--- react/src/app/components/Accommodations.tsx
+++ react/src/app/components/Accommodations.tsx
@@ -1,41 +1,9 @@
 'use client';
 
-import { useEffect, useState } from 'react';
-
-type ApiAccommodation = {
-	id: number;
-	name: string;
-	image: string;
-	restingCapacity: string;
-	rating: number;
-};
-
-type Accommodation = {
-	id: number;
-	name: string;
-	image: string;
-	restingCapacity: number;
-	rating: number;
-};
+import { useAccommodations } from './useAccommodations';
 
 export function Accommodations() {
-	const [data, setData] = useState<Accommodation[]>([]);
-
-	useEffect(() => {
-		const getData = async () => {
-			const result = await fetch('/api/accommodations');
-			const { data } = (await result.json()) as { data: ApiAccommodation[] };
-
-			const normalizedData = data.map((item) => ({
-				...item,
-				restingCapacity: Number(item.restingCapacity.split(' pessoas')[0]),
-			}));
-
-			setData(normalizedData);
-		};
-
-		getData();
-	}, []);
+	const { data } = useAccommodations();
 
 	return (
 		<div className="flex gap-4 flex-wrap justify-center">
---
+++ react/src/app/components/useAccommodations.ts
@@ -0,0 +1,41 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+type ApiAccommodation = {
+	id: number;
+	name: string;
+	image: string;
+	restingCapacity: string;
+	rating: number;
+};
+
+type Accommodation = {
+	id: number;
+	name: string;
+	image: string;
+	restingCapacity: number;
+	rating: number;
+};
+
+export function useAccommodations() {
+	const [data, setData] = useState<Accommodation[]>([]);
+
+	useEffect(() => {
+		const getData = async () => {
+			const result = await fetch('/api/accommodations');
+			const { data } = (await result.json()) as { data: ApiAccommodation[] };
+
+			const normalizedData = data.map((item) => ({
+				...item,
+				restingCapacity: Number(item.restingCapacity.split(' pessoas')[0]),
+			}));
+
+			setData(normalizedData);
+		};
+
+		getData();
+	}, []);
+
+	return { data };
+}
